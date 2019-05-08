343938f4771e728c651c6737108b649213a50a00
Parneet Kaur <742279@student.tor.loc>
2019-05-08 10:53:28 -0400

    10:52 commit

--- C0742279/C0742279/Program.cs
+++ C0742279/C0742279/Program.cs
@@ -10,6 +10,19 @@ namespace C0742279
     {
         static void Main(string[] args)
         {
+
+            int a ,b ;
+
+            Console.Write("Display the multiplication table:\n");
+           
+            Console.Write("Input the number to be calculated : ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            for (a = 1; a <= 10; a++)
+            {
+                Console.Write("{0} X {1} = {2} \n", a, b, a * b);
+            }
+            Console.ReadLine();
         }
     }
 }
